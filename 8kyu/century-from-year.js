d2e8eb1f93f6d939ef664bdcc5e93292e95b93a9
Cato13 <catobazzz@gmail.com>
2023-02-19 10:03:34 +0300

    other solution

--- 8kyu/century-from-year.js
+++ 8kyu/century-from-year.js
@@ -9,3 +9,16 @@ function century(year) {
     // Finish this :)
     return Math.ceil(year/100);
   }
+
+// or 
+
+function century(year) {
+  let century = 0;
+  
+  for(let i = 0; i < year; i++) {
+    if(i % 100 == 0) {
+      century++;
+    }
+  }
+  return century;
+}
