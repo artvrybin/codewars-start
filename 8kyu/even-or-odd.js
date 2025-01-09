c6690b2fa885f02741d0064c1fe02112314d2462
Artem Rybin <catobazzz@gmail.com>
2025-01-09 16:34:20 +0300

    even-or-odd.js upd

--- 8kyu/even-or-odd.js
+++ 8kyu/even-or-odd.js
@@ -1,10 +1,15 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
+// function evenOrOdd(number) {
+//     if (number % 2) {
+//         return "Odd";
+//     } else {
+//         return "Even";
+//     }
+// }
+
 function evenOrOdd(number) {
-    if (number % 2) {
-      return "Odd";
-    } else {
-      return "Even";
-    }
-    
-  }
+    return number % 2 && 'Odd' || 'Even'
+}
+
+console.log(evenOrOdd(10))
